2a01da6101b932ae6de44b563eeff95723ed19f1
lumin <luminesty@gmail.com>
2017-05-26 21:46:40 +0900

    djWjrnwjWjrn

--- lumin/test.py
+++ lumin/test.py
@@ -1,9 +1,18 @@
 import json
+import clstm
+import shorttext
+import sys
+
+reload(sys)
+sys.setdefaultencoding('utf8')
 
 data = {}
+cnt = 0
 with open('../data/yelp_academic_dataset_review.json') as f:
     for line in f:
+        cnt += 1
         star = json.loads(line)['stars']
+        review = json.loads(line)['text'].encode('utf-8')
         if star == 1:
             temp = 'very negative'
         elif star == 2:
@@ -16,7 +25,30 @@ with open('../data/yelp_academic_dataset_review.json') as f:
             temp = 'very positive'
 
         if temp in data:
-            data[temp].append(json.loads(line)['text'])
+            data[temp].append(review)
         else:
-            data[temp] = [json.loads(line)['text']]
+            data[temp] = [review]
+
+        if cnt == 10000:
+            break
+
+        
+print('wvmodel starts')
+wvmodel = shorttext.utils.load_word2vec_model('../data/GoogleNews-vectors-negative300.bin.gz')
+print('wvmodel closed')
+print('clstm starts')
+kmodel = clstm.CLSTMWordEmbed(len(data.keys()))
+print('clstm closed')
+print(kmodel)
+print('classifier starts')
+classifier = shorttext.classifiers.VarNNEmbeddedVecClassifier(wvmodel)
+classifier.train(data, kmodel)
+print('classifier closed')
+print(classifier.score('fuck you'))
+
+# print("very negative:", len(data['very negative']))
+# print("negative:", len(data['negative']))
+# print("neutral:", len(data['neutral']))
+# print("positive:", len(data['positive']))
+# print("very positive:", len(data['very positive']))
         
