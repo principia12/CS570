750f82547c7324fe3d6f93b016245842f892c0d4
han0ah <han0ah92@gmail.com>
2017-05-31 19:08:26 +0900

    final version of C-LSTM + GRNN

--- kijong/C-GRNN.py
+++ kijong/C-GRNN.py
@@ -4,45 +4,16 @@ import numpy as np
 import gensim
 import random
 
-from keras.layers import Convolution1D, MaxPooling1D, Dropout, LSTM, Dense, Flatten, GRU, Reshape
+from keras.layers import Convolution1D, MaxPooling1D, Dropout, LSTM, Dense,  GRU, GlobalAveragePooling1D
 from keras.models import Sequential
 from keras.regularizers import l2
 from keras import optimizers
 
 
-BATCH_SIZE = 500
+BATCH_SIZE = 200
+max_len = 350
+NB_LABLES = 10
 w2vecmodel = None
-
-def parseText(filename, nb_lables=5):
-
-    sentences = []
-    labels = []
-    count = 0
-    with open(filename, 'r', encoding='utf8') as f:
-        for line in f.readlines():
-            count += 1
-            temp = line.strip().split('\t\t')
-            rating = int(temp[2])
-
-            review = temp[3].replace('<sssss>', '').lower()
-
-            tokenizer = nltk.tokenize.RegexpTokenizer(r'\w+')
-            list_of_words = tokenizer.tokenize(review)
-
-            curr_labels = [0.0]*nb_lables
-            for i in range(nb_lables):
-                if (rating > 1):
-                    t = 111
-                if i == (rating-1):
-                    curr_labels[i] = 1.0
-                    break
-
-
-            sentences.append(list_of_words)
-            labels.append(curr_labels)
-
-    return (sentences, np.array(labels))
-
 
 def parse_text_to_review(path):
     data = []
@@ -63,7 +34,9 @@ def parse_text_to_review(path):
 
 def convert_trainingdata_matrix_yield(classdict):
     global BATCH_SIZE
-    classlabels = 5
+    global max_len
+    global NB_LABLES
+    classlabels = NB_LABLES
     cnt = 0
     while 1:
         random.shuffle(classdict)
@@ -72,7 +45,7 @@ def convert_trainingdata_matrix_yield(classdict):
             category_bucket[review[1]] = 1.0
 
             if cnt % BATCH_SIZE == 0:
-                x_train = np.zeros(shape=(BATCH_SIZE, 150, 300))
+                x_train = np.zeros(shape=(BATCH_SIZE, max_len, 300))
                 y_train = []
 
             x_train[cnt % BATCH_SIZE] = convert_to_w2vec_matrix([review[0]])[0]
@@ -84,10 +57,10 @@ def convert_trainingdata_matrix_yield(classdict):
                 yield (x_train, y_train)
 
 
-def convert_to_w2vec_matrix(sent_list,model=None, max_word_len=150):
-    global w2vecmodel
+def convert_to_w2vec_matrix(sent_list,model=None):
+    global max_len
     model = w2vecmodel
-    train_x = np.zeros((len(sent_list),max_word_len,300))
+    train_x = np.zeros((len(sent_list),max_len,300))
     i = 0
     for sents in sent_list:
         j = 0
@@ -95,7 +68,7 @@ def convert_to_w2vec_matrix(sent_list,model=None, max_word_len=150):
             if (word in model.vocab):
                 train_x[i][j] = model[word]
             j += 1
-            if (j >= max_word_len):
+            if (j >= max_len):
                 break
     return train_x
 
@@ -103,7 +76,6 @@ def convert_to_w2vec_matrix(sent_list,model=None, max_word_len=150):
 def C_GRNN(nb_labels,
            nb_filters=300,
            n_gram=3,
-           maxlen=150,
            vecsize=300,
            cnn_dropout=0.5,
            nb_rnnoutdim=300,
@@ -112,19 +84,23 @@ def C_GRNN(nb_labels,
            dense_wl2reg=0.005,
            optimizer='adam'):
 
+    global max_len
 
     model = Sequential()
     model.add(Convolution1D(nb_filter=nb_filters,
                             filter_length=n_gram,
                             border_mode='valid',
                             activation='relu',
-                            input_shape=(maxlen, vecsize)))
+                            input_shape=(max_len, vecsize)))
     if cnn_dropout > 0.0:
         model.add(Dropout(cnn_dropout))
     print ("after convolution output : ", model.output_shape)
-    model.add(MaxPooling1D(pool_length=maxlen - n_gram + 1))
-    print("after pooling output : ", model.output_shape)
-    model.add(GRU(nb_rnnoutdim))
+    #model.add(MaxPooling1D(pool_length=max_len - n_gram + 1))
+    #print("after pooling output : ", model.output_shape)
+    model.add(GRU(nb_rnnoutdim, return_sequences=True))
+    print("after gru output : ", model.output_shape)
+    model.add(GlobalAveragePooling1D())
+    print("after pooling : ", model.output_shape)
 
     if rnn_dropout > 0.0:
         model.add(Dropout(rnn_dropout))
@@ -143,13 +119,16 @@ def C_GRNN(nb_labels,
 def main():
     global w2vecmodel
     global BATCH_SIZE
+    global NB_LABLES
 
-    classdict = parse_text_to_review('../data/yelp-2013-train.txt.ss')
-    #classdict = classdict[0:1000]
+    #classdict = parse_text_to_review('../data/yelp-2013-train.txt.ss')
+    classdict = parse_text_to_review('../data/imdb-train.txt.ss')
     total_train = len(classdict)
-    testdict = parse_text_to_review('../data/yelp-2013-test.txt.ss')
-    #testdict = testdict[0:1000]
+
+    #testdict = parse_text_to_review('../data/yelp-2013-test.txt.ss')
+    testdict = parse_text_to_review('../data/imdb-test.txt.ss')
     total_test = len(testdict)
+
 
     print ('Data load finished')
 
@@ -157,20 +136,17 @@ def main():
     print( 'word2vec load finished...')
 
 
-    wl2reg_range = [0.001, 0.005, 0.01]
-    dropout_val_range = [0.2, 0.5]
-    nb_filters_range = [150, 300]
+    wl2reg_range = [0.001]
+    dropout_val_range = [0.5]
+    nb_filters_range = [150]
 
     score_results = []
 
     for dropout_val in dropout_val_range:
         for reg_val in wl2reg_range:
             for nb_filters_val in nb_filters_range:
-                if (reg_val == 0.005 and dropout_val == 0.5 and nb_filters_val == 300):
-                    continue
-                if (reg_val == 0.01 and dropout_val == 0.2 and nb_filters_val == 300):
-                    continue
-                model = C_GRNN(nb_labels=5,
+                model = C_GRNN(nb_labels=NB_LABLES,
+                               n_gram=3,
                                nb_filters=nb_filters_val,
                                nb_rnnoutdim=nb_filters_val,
                                cnn_dropout=dropout_val,
@@ -190,7 +166,7 @@ def main():
                     score = model.predict(x)[0]
                     max_prob = 0.0
                     max_class = None
-                    for i in range(5):
+                    for i in range(NB_LABLES):
                         if max_prob < score[i]:
                             max_prob = score[i]
                             max_class = i
@@ -199,7 +175,7 @@ def main():
                         cnt_correct += 1
                     cnt_all += 1
 
-                    if(cnt_all % 2000 == 0):
+                    if(cnt_all % 1000 == 0):
                         print("%d test finished.."%(cnt_all))
 
 
